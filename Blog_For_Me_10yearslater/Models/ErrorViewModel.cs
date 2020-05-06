83910d16909be1342c9bb2f016ac3d0da706bac3
mai1121 <maimaiukawa@gmail.com>
2020-05-06 20:46:15 +0900

    first commit

---
+++ Blog_For_Me_10yearslater/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Blog_For_Me_10yearslater.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
